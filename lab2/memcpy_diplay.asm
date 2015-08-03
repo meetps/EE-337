e48a5c17fafe1225d6f1e3cad6dae73527bc7d30
meetshah1995 <meetshah1995@iitb.ac.in>
2015-08-03 22:58:21 +0530

    Add Lab 2 Code

--- lab2/memcpy_diplay.asm
+++ lab2/memcpy_diplay.asm
@@ -4,107 +4,128 @@
 ; Contact  : meetshah1995@ee.iitb.ac.in
 ;-----------------------------------------
 
+ORG 0000H
+
+LJMP MAIN
+
+ORG 100H
+
+
+
 ;-----------------------------------------
 ; Sub Routine to generate delay of (d/2)s
 ;-----------------------------------------
 delay :
 CLR A
 MOV A,4FH ; get D from 4FH
-MUL A,#10 ; Mul by 10 as (d/2)s = 50*Dms     
+MOV B,#0AH
+MUL AB ; Mul by 10 as (d/2)s = 50*Dms     
 MOV R3,A  ; Mov the loop parameter to R3
 delayLoop : ; write loop to last d/2 seconds 
-			MOV R2,#200
+			MOV R4,#200
 			BACK1:
-			MOV R1,#0FFH	
+			MOV R5,#0FFH	
 			BACK :
-			DJNZ R1, BACK
-			DJNZ R2, BACK1
+			DJNZ R5, BACK
+			DJNZ R4, BACK1
 			DJNZ R3, delayLoop
 RET
 
+
+;-----------------------------------------
+; Sub Routine to erase sonsecutive memory
+;-----------------------------------------
 zeroOut :
 MOV R1, 50H   ; read fron 50H 
-MOV R2, 51H   ; read from 51H
-eraseLoop : MOV @R2,#00H  ; clear from P to P + N -1
-			INC R2        ; increment R2
+MOV R0, 51H   ; read from 51H
+eraseLoop : MOV @R0,#00H  ; clear from P to P + N -1
+			INC R0        ; increment R2
 			DJNZ R1, eraseLoop ; repeat until N
+RET
 
+
+;-----------------------------------------
+; Sub Routine to display LSB to P1
+;-----------------------------------------
 display :
 MOV R1,50H	  ; read fron 50H
-MOV R2,51H    ; read from 51H
-readLoop : MOV R3,@R2 ; read from P to P+N -1 
-           MOV P1,R3  ; display last 4 bits
-           CLR P1.0
-           CLR P1.1
-           CLR P1.2
-           CLR P1.3
-           MOV 4FH,#02 ; Setting D =2 for 1s delay
-		   delay      ; delay 1s
-		   INC R2
+MOV R0,51H    ; read from 51H
+readLoop : MOV A,@R0 ; read from P to P+N -1 
+           SWAP A
+		   ANL A,#0F0H
+		   MOV P1,A  ; display last 4 bits
+           ;MOV 4FH,#02 ; Setting D =2 for 1s delay
+		   LCALL delay      ; delay 1s
+		   INC R0
 		   DJNZ R1, readLoop
 RET
 
+
+;-----------------------------------------
+; Sub Routine to store squareSum to memory
+;-----------------------------------------
 sumOfSquares :
-MOV R1,50H	  ; read N fron 50H
-MOV R2,51H    ; read P from 51H
-MOV R3,#01  
+MOV R0,51H    ; read P from 51H
+MOV R2,50H	  ; read N fron 50H
+MOV R3,#01H
+MOV @R0,#01
 ; get sumOfSquares till i 
 squareStoreLoop :
 		   CLR A
-		   MOV A,R3
-		   MUl A,R3
-		   MOV R3,A
-		   MOV A,R2
-		   ADD A,R3
-		   MOV @R2,A ; store in i from P to P + N -1
 		   INC R3
-		   INC R2
-		   DJNZ R1, squareStoreLoop
+		   MOV A,R3
+		   MOV B,R3
+		   MUL AB
+		   MOV R4,A
+		   MOV A,@R0
+		   ADD A,R4
+		   INC R0
+		   MOV @R0,A ; store in i from P to P + N -1
+		   DJNZ R2, squareStoreLoop
 RET
-
 ; LabWork 
 
+
+
+;-----------------------------------------
+; Sub Routine to copy cons. memory units
+;-----------------------------------------
 memcpy :
-MOV R1, 50H		; read N from 50H
-MOV R2, 51H		; read A from 51H
-MOV R3, 52H		; read B from 52H
+MOV R0, 51H		; read A from 51H
+MOV R1, 52H		; read B from 52H
+MOV R2, 50H		; read N from 50H
 copyLoop :      ; copy N locations consecutively from A to B
-		   MOV A,@R2
-		   MOV @R3,A
-		   INC R2
-		   INC R3
-		   DJNZ R1, memcpy
-
-;main_lab :
-; zerOut(A)
-; zerOut(B)
-; sumOfSquares(A)
-; memcpy(A,B)
-; dsiplay(B)
-
-; return
+		   MOV A,@R0
+		   MOV @R1,A
+		   INC R0
+		   INC R1
+		   DJNZ R2, copyLoop
+RET
 
 
-ORG 0XXXH
+
+;-----------------------------------------
+; Main 
+;-----------------------------------------
 MAIN:
 MOV SP,#0CFH;-----------------------Initialize STACK POINTER
-MOV 50H,#XX;------------------------N memory locations of Array A
-MOV 51H,#XX;------------------------Array A start location
+MOV 50H,#08;------------------------N memory locations of Array A
+MOV 51H,#70H;------------------------Array A start location
 LCALL zeroOut;----------------------Clear memory
-MOV 50H,#XX;------------------------N memory locations of Array B
-MOV 51H,#XX;------------------------Array B start location
+MOV 50H,#08;------------------------N memory locations of Array B
+MOV 51H,#60H;------------------------Array B start location
 LCALL zeroOut;----------------------Clear memory
-MOV 50H,#XX;------------------------N memory locations of Array A
-MOV 51H,#XX;------------------------Array A start location
+MOV 50H,#08;------------------------N memory locations of Array A
+MOV 51H,#70H;------------------------Array A start location
 LCALL sumOfSquares;-----------------Write at memory location
-MOV 50H,#XX;------------------------N elements of Array A to be copied in Array B
-MOV 51H,#XX;------------------------Array A start location
-MOV 52H,#XX;------------------------Array B start location
+MOV 50H,#08;------------------------N elements of Array A to be copied in Array B
+MOV 51H,#70H;------------------------Array A start location
+MOV 52H,#60H;------------------------Array B start location
 LCALL memcpy;-----------------------Copy block of memory to other location
-MOV 50H,#XX;------------------------N memory locations of Array B
-MOV 51H,#XX;------------------------Array B start location
-MOV 4FH,#XX;------------------------User defined delay value
+MOV 50H,#08;------------------------N memory locations of Array B
+MOV 51H,#60H;------------------------Array B start location
+MOV 4FH,#03H;------------------------User defined delay value
 LCALL display;----------------------Display the last four bits of elements on LEDs
-here:SJMP here;---------------------WHILE loop(Infinite Loop)
+;here:SJMP here;---------------------WHILE loop(Infinite Loop)
 END
 ;------------------------------------END MAIN------------------------------------------- 
